f2e93c86387a66a5c40e2bf01e8ffaf9c37617fd
Samuel Morgan <sam@morgan.dev>
2024-05-22 21:57:13 -0400

    get post updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "./styles/App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Work from "./pages/Work";
 import Contact from "./pages/Contact";
 import Layout from "./components/Layout";
+import WorkRoute from "./pages/Work";
 function App() {
 	return (
 		<div className="flex flex-col mb-5 mr-2">
@@ -14,7 +14,7 @@ function App() {
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/about" element={<About />} />
-						<Route path="/work" element={<Work />} />
+						<Route path="/work" element={<WorkRoute />} />
 						<Route path="/contact" element={<Contact />} />
 					</Routes>
 				</Layout>
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,69 +1,36 @@
-import type { FC } from "react";
+import { useEffect, useState, type FC } from "react";
+interface Post {
+	id: number;
+	title: string;
+	body: string;
+	image: string;
+	tags: string;
+	category: string;
+	created_at: string;
+	company_name: string;
+	company_logo: string;
+	company_description: string;
+	client: string;
+}
+interface WorkProps {
+	posts: Post[];
+}
 
-interface WorkProps {}
+const WorkRoute: FC = () => {
+	const [posts, setPosts] = useState<Post[]>([]);
+
+	useEffect(() => {
+		fetch(`${process.env.API_URL}/posts`)
+			.then((res) => res.json())
+			.then((data) => setPosts(data))
+			.catch((error) => console.error(error));
+	}, []);
+
+	return <WorkUI posts={posts} />;
+};
 
 // eslint-disable-next-line no-empty-pattern
-const Work: FC<WorkProps> = ({}) => {
-	const posts = [
-		{
-			id: 1,
-			title: "Boost your conversion rate",
-			href: "#",
-			description:
-				"Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.",
-			imageUrl:
-				"https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
-			date: "Mar 16, 2020",
-			datetime: "2020-03-16",
-			category: { title: "Marketing", href: "#" },
-			author: {
-				name: "Michael Foster",
-				role: "Co-Founder / CTO",
-				href: "#",
-				imageUrl:
-					"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-			},
-		},
-		{
-			id: 2,
-			title: "Boost your conversion rate",
-			href: "#",
-			description:
-				"Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.",
-			imageUrl:
-				"https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
-			date: "Mar 16, 2020",
-			datetime: "2020-03-16",
-			category: { title: "Marketing", href: "#" },
-			author: {
-				name: "Michael Foster",
-				role: "Co-Founder / CTO",
-				href: "#",
-				imageUrl:
-					"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-			},
-		},
-		{
-			id: 3,
-			title: "Boost your conversion rate",
-			href: "#",
-			description:
-				"Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.",
-			imageUrl:
-				"https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
-			date: "Mar 16, 2020",
-			datetime: "2020-03-16",
-			category: { title: "Marketing", href: "#" },
-			author: {
-				name: "Michael Foster",
-				role: "Co-Founder / CTO",
-				href: "#",
-				imageUrl:
-					"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-			},
-		},
-		// More posts...
-	];
+const WorkUI: FC<WorkProps> = ({ posts }) => {
 	return (
 		<div>
 			<h2 className="text-left lg:text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
@@ -80,7 +47,7 @@ const Work: FC<WorkProps> = ({}) => {
 						className="relative isolate flex flex-col gap-8 lg:flex-row">
 						<div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
 							<img
-								src={post.imageUrl}
+								src={post.image}
 								alt=""
 								className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
 							/>
@@ -88,41 +55,41 @@ const Work: FC<WorkProps> = ({}) => {
 						</div>
 						<div>
 							<div className="flex items-center gap-x-4 text-xs">
-								<time dateTime={post.datetime} className="text-gray-500">
-									{post.date}
+								<time dateTime={post.created_at} className="text-gray-500">
+									{post.created_at}
 								</time>
 								<a
-									href={post.category.href}
+									href={`/${post.category}`}
 									className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
-									{post.category.title}
+									{post.category}
 								</a>
 							</div>
 							<div className="group relative max-w-xl">
 								<h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-									<a href={post.href}>
+									<a href={`/${post.id}`}>
 										<span className="absolute inset-0" />
 										{post.title}
 									</a>
 								</h3>
 								<p className="mt-5 text-sm leading-6 text-gray-600">
-									{post.description}
+									{post.body}
 								</p>
 							</div>
 							<div className="mt-6 flex border-t border-gray-900/5 pt-6">
 								<div className="relative flex items-center gap-x-4">
 									<img
-										src={post.author.imageUrl}
+										src={post.company_logo}
 										alt=""
 										className="h-10 w-10 rounded-full bg-gray-50"
 									/>
 									<div className="text-sm leading-6">
 										<p className="font-semibold text-gray-900">
-											<a href={post.author.href}>
+											<a href={`/${post.company_name}`}>
 												<span className="absolute inset-0" />
-												{post.author.name}
+												{post.company_name}
 											</a>
 										</p>
-										<p className="text-gray-600">{post.author.role}</p>
+										<p className="text-gray-600">{post.company_description}</p>
 									</div>
 								</div>
 							</div>
@@ -134,4 +101,4 @@ const Work: FC<WorkProps> = ({}) => {
 	);
 };
 
-export default Work;
+export default WorkRoute;
